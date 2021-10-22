e4da5643444da57461d8a84e9dc1851165305b53
Ronnie Rios <ronniesmail14@gmail.com>
2021-10-21 22:55:55 -0500

    navbar and react router dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export const Navbar = () => {
     return (
-        <div>
+        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify:center items-center border-b dark:border-gray-700 border-gray-200">
+            <div className="flex justofy-between items-center space-x-5 w-screen">
+                <Link to='/'>
+                    <p className="text-2xl">
+                        Search
+                    </p>
+                </Link>
+            </div>
             Navbar
         </div>
     )
